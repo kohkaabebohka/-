fc9587767531c56a048b188fb9792abeaeaeef56
Николай <nikolajorlov043@gmail.com>
2022-12-16 14:41:42 +0300

    week

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a <= 0 || a >= 8)
+{
+    Console.Write("Такого дня не существует");
+}
+else
+{
+    if (a == 1) Console.Write("Понедельник");
+    if (a == 2) Console.Write("Вторник");
+    if (a == 3) Console.Write("Среда");
+    if (a == 4) Console.Write("Четверг");
+    if (a == 5) Console.Write("Пятница");
+    if (a == 6) Console.Write("Суббота");
+    if (a == 7) Console.Write("Воскресенье");
+}
